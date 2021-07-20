6bff3c1ccbd9048e55499108e9a0dd03bd2ff78c
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-07-20 14:06:25 +0300

    VS commit

--- ProjectCalendarPlanner/Controllers/ScheduleViewController.swift
+++ ProjectCalendarPlanner/Controllers/ScheduleViewController.swift
@@ -10,6 +10,8 @@ import FSCalendar
 
 class ScheduleViewController: UIViewController {
     
+    var calendarHeight: NSLayoutConstraint!
+    
     private var calendar: FSCalendar = {
         let calendar = FSCalendar()
         calendar.translatesAutoresizingMaskIntoConstraints = false
@@ -22,23 +24,43 @@ class ScheduleViewController: UIViewController {
         
         view.backgroundColor = .white
         title = "Schedule"
+        
+        calendar.delegate = self
+        calendar.dataSource = self
+            
         setupConstraints()
         
     }
 
 }
 
+// MARK: - FSCalendarDataSource, FSCalendarDelegate
+
+extension ScheduleViewController: FSCalendarDataSource, FSCalendarDelegate {
+    
+    
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeight.constant = bounds.height
+        view.layoutIfNeeded()
+    }
+}
+
 // MARK: - SetupConstraints
 
 extension ScheduleViewController {
     
+    
     func setupConstraints() {
+        
         view.addSubview(calendar)
+        
+        calendarHeight = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
+        calendar.addConstraint(calendarHeight)
+        
         NSLayoutConstraint.activate([
             calendar.topAnchor.constraint(equalTo: view.topAnchor, constant: 90),
             calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
             calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            calendar.heightAnchor.constraint(equalToConstant: 300)
         ])
     }
 }
