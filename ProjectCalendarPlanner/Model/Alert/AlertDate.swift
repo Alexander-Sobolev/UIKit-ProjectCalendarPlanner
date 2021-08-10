2477c367b599fed5e3eb29013c1fdb322358394c
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-08-10 22:02:50 +0300

    Alert

--- ProjectCalendarPlanner/Model/Alert/AlertDate.swift
+++ ProjectCalendarPlanner/Model/Alert/AlertDate.swift
@@ -9,7 +9,7 @@ import UIKit
 
 extension UIViewController {
     
-    func alertDate(label: UILabel, comHandler: (Int, Date) -> Void) {
+    func alertDate(label: UILabel, completionHandler: @escaping (Int, Date) -> Void) {
         
         let alert = UIAlertController(title: "", message: nil, preferredStyle: .actionSheet)
         
@@ -28,10 +28,30 @@ extension UIViewController {
             
             let calendar = Calendar.current
             let component = calendar.dateComponents([.weekday], from: datePicker.date)
+            guard let weekday = component.weekday else { return }
+            let numberWeekday = weekday
+            let date = datePicker.date
+            completionHandler(numberWeekday, date)
+            
+            label.text = dateString
             
         }
         
+        let cancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
         
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        
+        
+        alert.view.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        datePicker.widthAnchor.constraint(equalTo: alert.view.widthAnchor).isActive = true
+        datePicker.heightAnchor.constraint(equalToConstant: 160).isActive = true
+        datePicker.topAnchor.constraint(equalTo: alert.view.topAnchor, constant: 20).isActive = true
+        
+        
+        present(alert, animated: true, completion: nil)
         
     }
 }
