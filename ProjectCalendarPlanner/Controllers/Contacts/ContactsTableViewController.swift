17771d415e997529588cb88494ed7df00e80ed74
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-08-19 18:47:01 +0300

    Created ContactsController

---
+++ ProjectCalendarPlanner/Controllers/Contacts/ContactsTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ContactsTableViewController.swift
+//  ProjectCalendarPlanner
+//
+//  Created by Alexander Sobolev on 19.08.2021.
+//
+
+import UIKit
+
+class ContactsTableViewController: UITableViewController {
+    
+    let idContact = "idContactC"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = #colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.968627451, alpha: 1)
+        tableView.separatorStyle = .singleLine
+        tableView.bounces = false
+        tableView.register(ContactsTableViewCell.self, forCellReuseIdentifier: idContact)
+        
+        
+        title = "Contacts"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        
+    }
+    
+    @objc func addButtonTapped() {
+        
+        let scheduleOption = OptionsScheduleTableViewController()
+        navigationController?.pushViewController(scheduleOption, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: idContact, for: indexPath) as! ContactsTableViewCell
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            print("Tap OptionsTabelView")
+        }
+    
+}
